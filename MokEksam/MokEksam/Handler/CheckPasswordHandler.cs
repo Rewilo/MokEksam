2b7d9a85bb51427aa109b77d235dc03ed7d498b1
Oliwer Hansen <oliwer93@hotmail.com>
2016-10-07 11:37:59 +0200

    Login Almost

---
+++ MokEksam/MokEksam/Handler/CheckPasswordHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using MokEksam.Common;
+
+namespace MokEksam.Handler
+{
+    public class CheckPasswordHandler
+    {
+
+        public bool CheckPassword(string username, string password)
+        {
+            try
+            {
+                HttpClientHandler clientHandler = new HttpClientHandler();
+                clientHandler.UseDefaultCredentials = true;
+
+                using (HttpClient client = new HttpClient(clientHandler))
+                {
+                    client.BaseAddress = new Uri(Config.ConnectionUrl);
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("applicantion/json"));
+
+
+                    var result =
+                        client.GetAsync("api/Password?endUserName=" + username + "&password=" + password).Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        return true;
+                    }
+                    if (result.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        return false;
+                    }
+                    throw new ArgumentException("Something went wrong along the way... Try another day");
+                }
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MokEksam/MokEksam/ViewModel/LogInViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+using MokEksam.Common;
+using MokEksam.Handler;
+
+namespace MokEksam.ViewModel
+{
+    public class LogInViewModel
+    {
+        private ICommand _logInCommand;
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public CheckPasswordHandler Handler { get; set; }
+
+        public ICommand LogInCommand
+        {
+            get { return _logInCommand ?? (_logInCommand = new RelayCommand(LogIn)); }
+            set { _logInCommand = value; }
+        }
+
+        public async void LogIn()
+        {
+            
+
+            try
+            {
+                Handler.CheckPassword(Username, Password);
+
+
+                // login success
+            }
+            catch (UnSuccesfulRequest)
+            {
+                // login fail
+
+
+            }
+        }
+
+        public LogInViewModel()
+        {
+            LogInCommand = new RelayCommand(LogIn);
+            Handler = new CheckPasswordHandler();
+        }
+    }
+}
