5921eb775f9ec5c9d7f88f732c482b7f556aff51
rasm547h <rasm547h@edu.easj.dk>
2016-10-14 10:24:53 +0200

    I have been cleaning up in the Solution and fixed a bug in the LogIn.xaml

--- MokEksam/ServerDatabase/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ServerDatabase.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
